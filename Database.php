895e38155316da4b232b2edb487373aeaed78054
PJMoller <peter.moller@student.nhlstenden.com>
2024-01-14 21:26:45 +0100

    first commit

---
+++ Database.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio</title>
+    <link rel="icon" href="./img/frog-spinning.gif" type="image/gif"/>
+    <link rel="stylesheet" href="./CSS/style.css">
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <a href="index.html"><img src="./img/logo.jpg" alt="Logo"></a>
+            <h1>Peter's portfolio!</h1>
+        </div>    
+            <nav>
+                <ul>
+                    <li><a href="PS.html">PS</a></li>
+                    <li><a href="Database.php">Databases</a></li>
+                </ul>
+            </nav>
+    </header>  
+    <main>
+        <h2>Give me an image!</h2>
+        <form action="#">
+        <input type="file" id="myFile" name="filename">
+        <input type="submit">
+        </form>
+    </main>
+
+    <footer>
+
+
+    </footer>
+
+
+
+</body>
+</html>
---
+++ Reportbug.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio</title>
+    <link rel="icon" href="./img/frog-spinning.gif" type="image/gif"/>
+    <link rel="stylesheet" href="./CSS/style.css">
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <a href="index.html"><img src="./img/logo.jpg" alt="Logo"></a>
+            <h1>Peter's portfolio!</h1>
+        </div>    
+            <nav>
+                <ul>
+                    <li><a href="PS.html">PS</a></li>
+                    <li><a href="Database.php">Databases</a></li>
+                </ul>
+            </nav>
+    </header>  
+    <main>
+        <h2>Report a bug!</h2>
+        <form action="#" method="POST">
+            <input type="text" name="product" id="product" required placeholder="Product">
+            <label for="product">Product</label>
+            <input type="text" name="version" id="version" required placeholder="Version">
+            <label for="verion">Version</label>
+            <input type="text" name="hardware" id="hardware" required placeholder="Hardware">
+            <label for="hardware">Hardware</label>
+            <input type="text" name="os" id="os" required placeholder="OS">
+            <label for="os">OS</label>
+            <input type="text" name="frequency" id="frequency" required placeholder="Frequency">
+            <label for="frequency">Frequency</label>
+            <input type="text" name="frequency" id="frequency" placeholder="olution">
+            <label for="olution">Solution</label>
+            
+
+        </form>
+    </main>
+
+    <footer>
+
+
+    </footer>
+
+
+
+</body>
+</html>
